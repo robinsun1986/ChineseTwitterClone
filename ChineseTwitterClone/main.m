53b312053d19c86a095a6571edaab77b93404784
robinsun <s3441078@student.rmit.edu.au>
2014-08-27 12:06:10 +1000

    Initial Commit

---
+++ ChineseTwitterClone/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ChineseTwitterClone
+//
+//  Created by wilson on 8/27/14.
+//  Copyright (c) 2014 robin.sun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
